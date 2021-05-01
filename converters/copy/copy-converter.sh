1c3eee344dd0ed2a9468ce1f5fc942fa2c1a819a
Benjamin Poldrack <benjaminpoldrack@gmail.com>
2021-05-01 16:06:35 +0200

    BF: copy-converter now follows symlinks in source
    copy-converter got a third argument follow|dont-follow defaulting
    to 'follow'. Not following by default defies the standard purpose of
    copying files from source dataset.

--- converters/copy/copy-converter.sh
+++ converters/copy/copy-converter.sh
@@ -1,14 +1,30 @@
 #!/usr/bin/env bash
 # simply copy a file and add it to destination dataset
+#
+# Usage: <script> <source> <destination> <follow|dont-follow>
+#
+# Default: follow
+#          follow links in <source> in order to copy content rather than the
+#          link
+#
+#
 # TODO: a bit of protection for too many/less arguments ...
 
 
 set -e -u
 set -x
 
+# TODO: Better do proper option evaluation.
+if [[ ${3:-follow} == "follow" ]];
+then
+    follow_opt="-L"
+else
+    follow_opt=""
+fi
+
 if [ -d "$1" ]
 then
-    datalad run --explicit --input "$1" --output "$2"  cp -r "$1" "$2"
+    datalad run --explicit --input ${1} --output ${2}  cp ${follow_opt} -r ${1} ${2}
 else
-    datalad run --explicit --input "$1" --output "$2"  cp "$1" "$2"
+    datalad run --explicit --input ${1} --output ${2}  cp ${follow_opt} ${1} ${2}
 fi
