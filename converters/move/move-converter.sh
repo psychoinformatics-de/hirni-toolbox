fd2166f48dbbf3cfc17186a79c02894ef25c8ca1
Benjamin Poldrack <benjaminpoldrack@gmail.com>
2021-05-01 16:51:25 +0200

    TMP: Test mv converter

--- converters/move/move-converter.sh
+++ converters/move/move-converter.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
-# simply git-move a file
+# simply move a file
+#
+# Attention: This is intended for use within target dataset.
+#
+# Needs a thorough consideration whether/how to generalize
 # TODO: a bit of protection for too many/less arguments ...
 
 set -e -u
 set -x
 
-datalad run --explicit --input "$1" --output "$2" git mv "$1" "$2"
+datalad run --explicit --input "$1" --output "$2" mv "$1" "$2"
